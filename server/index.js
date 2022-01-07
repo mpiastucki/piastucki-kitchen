e2c8d095e224db7c1f8575ff3346da0f8e4dcaa9
Michael Piastucki <michael.piastucki@woven-planet.global>
2022-01-07 11:56:15 +0900

    Use dotenv for env variables

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
-const port = 9000;
+const port = process.env.PORT;
 const db = require('./db')
 
 
+app.get('/', (req, res) => {
+    res.send("use '/api/all' URL to retrieve recipes")
+})
 
-
-app.get("/", (req, res) => {
-    db.db.get('1').then(function (doc) {
-        res.send(JSON.stringify(doc.name))
-    })
+app.get("/api/all", (req, res) => {
+    db.db.allDocs({include_docs:true}).then(function (allDocs) {
+        res.send(JSON.stringify(allDocs))
+    }).catch(function(err){res.status(500);})
 })
 
 
