8f650c49a9efb63169e703fdf93d54c5d61ccd8a
Michael Piastucki <michael.piastucki@woven-planet.global>
2022-01-17 13:32:07 +0900

    stringify data and send

--- server/index.js
+++ server/index.js
@@ -1,20 +1,23 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
+const port = process.env.REST_PORT;
 const db = require('./db')
 
 
-app.get('/', (req, res) => {
-    res.send("use '/api/all' URL to retrieve recipes")
-})
+
 
 app.get("/api/all", (req, res) => {
     db.db.allDocs({include_docs:true}).then(function (allDocs) {
-        res.send(JSON.stringify(allDocs))
+        res.status(200).send(JSON.stringify(allDocs.rows))
     }).catch(function(err){res.status(500);})
 })
 
+app.use(express.static("dist"))
+
+app.get("/", (req, res) => {
+    res.status(200)
+})
 
 app.listen(port, () => {
     console.log(`Listening on ${port}`)
