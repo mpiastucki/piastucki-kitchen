715afbd04131743e6ffb2d11fcb56d2e6a49f71c
Michael Piastucki <michael.piastucki@woven-planet.global>
2022-01-17 13:32:27 +0900

    fetch data and display recipes

--- src/components/RecipeList.vue
+++ src/components/RecipeList.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="recipe-list">
         <ul>
-            <li v-for="recipe in recipes" :key="recipe.id">{{recipe.name}}</li>
+            <li v-for="recipe in recipes" :key="recipe.id">{{recipe.doc.name}}</li>
         </ul>
     </div>
 </template>
@@ -10,8 +10,11 @@
 export default {
     data(){
         return {
-            "recipes":[{name:"curry rice", id:"1"}]
+            "recipes":null
         }
+    },
+    mounted() {
+        fetch("http://localhost:9000/api/all").then(response => response.json()).then(recipesArray => this.recipes=recipesArray).catch((err) => {console.log(err)})
     },
 }
 </script>
