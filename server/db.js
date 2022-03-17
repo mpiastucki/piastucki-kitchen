c044a064dbb8b1279c061076fcd9c0d35808cf4d
mpiastucki <michael.piastucki@woven-planet.global>
2022-03-17 11:55:47 +0900

    refactor to use postgres

--- server/db.js
+++ server/db.js
@@ -1,3 +1,59 @@
-const pouchdb = require('pouchdb');
+const {Sequelize, Datatypes, Model} = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_URI);
 
-module.exports = {db : new pouchdb('recipes-db')}
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+})();
+
+const Recipe = sequelize.define('Recipe', {
+    id: {
+        type: Datatypes.UUID,
+        primaryKey: true,
+        defaultValue: Datatypes.UUIDV1,
+    },
+    name: {
+        type: Datatypes.STRING,
+        allowNull: false,
+    },
+})
+
+const RecipeIngredients = sequelize.define('RecipeIngredients', {
+    id: {
+        type: Datatypes.UUID,
+        defaultValue:Datatypes.UUIDV1,
+        primaryKey: true,
+    },
+    recipe: {
+        type: Datatypes.UUID,
+        references: {
+            model: Recipe,
+            key:"id"
+        }
+    },
+    ingredient: {
+        type: Datatypes.UUID,
+        references: {
+            model: Ingredient,
+            key: "name"
+        }
+    }
+
+}
+)
+
+const Ingredient = sequelize.define('Ingredient', {
+    //TODO: make ingredient table
+    id: {
+        type: Datatypes.UUID,
+        defaultValue: Datatypes.UUIDV1,
+        },
+    name: {
+        type: Datatypes.STRING,
+        primaryKey: true
+        },
+})
