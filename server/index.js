30c885d6171612940e261f72afc93c694381d091
mpiastucki <michael.piastucki@woven-planet.global>
2022-03-25 11:55:09 +0900

    add GET all endpoint

--- server/index.js
+++ server/index.js
@@ -1,46 +1,24 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
-const port = process.env.REST_PORT;
-const {db} = require('./db')
-
+const port = process.env.PORT;
+const {sequelize, Recipe, Ingredient} = require('./db')
 
 
 
 app.get("/api/all", (req, res) => {
-    db.allDocs({include_docs:true}).then(function (allDocs) {
-        res.status(200).send(JSON.stringify(allDocs.rows))
-    }).catch(function(err){res.status(500);})
+    //TODO: refactor to use sequelize
+    Recipe.findAll().then(allRecipes => JSON.stringify(allRecipes)).then(json => res.status(200).send(json));
 })
 
 app.get("/api/threerecipes", (req, res) => {
-    db.allDocs({include_docs:true}).then(function(alldocs) {
-        const threeRecipes = [];
-        while (threeRecipes.length < 3) {
-            let recipe = alldocs.rows[Math.floor(Math.random() * alldocs.rows.length)];
-            if(!threeRecipes.includes(recipe.id)) {
-                threeRecipes.push(recipe.id);
-            }
-        }
-        return threeRecipes;
-    }).then(function(threeRecipes){res.status(200).send(JSON.stringify(threeRecipes))}).catch(function(err) {console.log(err)})
+    //TODO: refactor to use sequelize
+    
 })
 
 app.use(express.urlencoded({ extended: true }))
 app.post("/api/addrecipe", (req, res) => {
-    const body = req.body;
-    const doc = {
-        "_id": body["recipe-name"],
-        "ingredient1": body["ingredient1"] ? body["ingredient1"] : null,
-        "ingredient2": body["ingredient2"] ? body["ingredient2"] : null,
-        "ingredient3": body["ingredient3"] ? body["ingredient3"] : null,
-        "breakfast": body["breakfast"] ? body["breakfast"] : false,
-        "lunch": body["lunch"] ? body["lunch"] : false,
-        "dinner": body["dinner"] ? body["dinner"] : false,
-    };
-
-    db.put(doc);
-    res.redirect(302, "/")
+    //TODO: refactor to use sequelize
 
 })
 
